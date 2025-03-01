ba9abfe81ac00a981279db8ba7ffa04ee113b382
davemccann <davemccann@users.noreply.github.com>
2025-03-01 00:58:53 +0000

    Modified build script to not run tests if a build fails

--- build.bat
+++ build.bat
@@ -24,16 +24,15 @@ if %type%=="build" (
 ) else if %type%=="run" (
 	echo Building and running the project...
 	premake5 gmake
-	gnumake config=debug
+	gnumake config=debug || exit /b 1
+	set EXITCODE=%ERRORLEVEL%	
 	%execpath%
-
 	exit /b 0
 ) else if %type%=="debug" (
 	echo Building and running using debugger...
 	premake5 gmake
-	gnumake config=debug
+	gnumake config=debug || exit /b 1
 	raddbg --run %execpath%
-
 	exit /b 0
 ) else (
 	echo Invalid argument: %type%
